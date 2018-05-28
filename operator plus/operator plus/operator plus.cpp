ba3045d5823f306889efd72664eeadcdc44796af
20172100386 <39482031+20172100386@users.noreply.github.com>
2018-05-28 17:41:07 +0800

    改成减法
    错的程序v

--- operator plus/operator plus/operator plus.cpp
+++ operator plus/operator plus/operator plus.cpp
@@ -11,7 +11,7 @@ private:
 public:
 	void setvalue(int fe, int in);
 	void display();
-	CFeet operator + (CFeet &objf);
+	CFeet operator - (CFeet &objf);
 	CFeet add(CFeet &objf);
 };
 void CFeet::setvalue(int fe, int in)
@@ -21,18 +21,32 @@ void CFeet::setvalue(int fe, int in)
 }
 void CFeet::display()
 {
-	cout << "Ó¢³ß=  " << feet << "Ó¢´ç=  " << inches << endl;
+	cout << "Ó¢³ß=  " << feet << "  Ó¢´ç=  " << inches << endl;
 }
-CFeet CFeet::add(CFeet &objf)
+/*CFeet CFeet::add(CFeet &objf)
 {
 	CFeet temp;
-	temp.setvalue(feet + objf.feet, inches + objf.inches);
+	temp.setvalue(feet - objf.feet, inches - objf.inches);
 	return temp;
-}
-CFeet CFeet::operator +(CFeet &objf)
+}*/
+CFeet CFeet::operator -(CFeet &objf)
 {
 	CFeet temp;
-	temp.setvalue(feet + objf.feet, inches + objf.inches);
+	int t;
+	if (feet < 0)
+	{
+		t = inches + objf.inches;
+	}
+	else if (inches > objf.inches)
+	{
+		t = inches - objf.inches;
+	}
+	else if(inches<objf.inches)
+	{
+		t = objf.inches - inches;
+	}
+	
+	temp.setvalue(feet - objf.feet, t);
 	return temp;
 }
 
@@ -40,9 +54,9 @@ CFeet CFeet::operator +(CFeet &objf)
 int main()
 {
 	CFeet A, B, C;
-	A.setvalue(9, 8);
-	B.setvalue(10, 12);
-	C = A + B;
+	A.setvalue(1, 3);
+	B.setvalue(2, 2);
+	C = A - B;
 	C.display();
 	return 0;
 }
